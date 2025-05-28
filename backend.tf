27dc95c08f02813e5a65cc07d96c602119b3bdff
root <root@ip-172-31-22-53.ap-south-1.compute.internal>
2025-05-28 11:01:03 +0000

    Fix: Format Terraform files

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "khalidsuhails3"
-    key    = "state"
-    region = "ap-south-1"
+    bucket         = "khalidsuhails3"
+    key            = "state"
+    region         = "ap-south-1"
     dynamodb_table = "state-lock"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "region" {
-  type = string
+  type    = string
   default = "ap-south-1"
 }
